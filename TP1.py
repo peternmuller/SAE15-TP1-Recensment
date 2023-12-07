7d5509717e05612ef8fe7d166ae2f2699bd700b6
Skallr2 <pm563838@gmail.com>
2023-12-07 16:18:36 +0100

    Mise à jour du code python

--- TP1.py
+++ TP1.py
@@ -1,43 +1,68 @@
 import csv
+from pylab import *
 
-def importation(fichier, separateur):
-    liste = []
-    with open(fichier, newline='', encoding="UTF-8") as csvfile:
-        reader = csv.reader(csvfile, delimiter=separateur)
-        for row in reader:
-            liste.append(row)
-    return liste
+def importation(fichier,separateur):									#permet d'importer un csv dans une liste en précisant le fichier et le séparateur
+	liste=[]
+	with open(fichier,newline='',encoding="UTF-8") as csvfile:
+		reader=csv.reader(csvfile,delimiter=separateur)
+		for row in reader:
+			liste.append(row)
+	return liste
 
-donnees_2008 = importation("donnees_2008.csv", ",")
-donnees_2016 = importation("donnees_2016.csv", ",")
-donnees_2021 = importation("donnees_2021.csv", ";")
-metadonnees_communes = importation("metadonnees_communes.csv", ";")
+donnees_2008 = importation("donnees_2008.csv",",")
+donnees_2016 = importation("donnees_2016.csv",",")
+donnees_2021 = importation("donnees_2021.csv",";")
+metadonnees_communes = importation("metadonnees_communes.csv",";")
+#On a maintenant 4 listes qui contiennent toutes les infos des fichiers
 
-def selec_region(donnees, annee, code_dep, meta, agglo):
-    liste = []
-    if annee < 2020:
-        for ligne in donnees:
-            if ligne[2].isdigit() and int(ligne[2]) == code_dep and ligne[6] in agglo:
-                liste.append([ligne[6], int(ligne[7]), int(ligne[8]), int(ligne[9])])
-    else:
-        for ligne in donnees:
-            if ligne[1].isdigit() and int(ligne[1]) == code_dep:
-                for communes in meta:
-                    if ligne[2] == communes[2] and communes[3] in agglo:
-                        liste.append([communes[3], int(ligne[3]), int(ligne[4]), int(ligne[5])])
-    return liste
+def selec_region(donnees, annee, code_dep, meta, agglo):				#permet de ne garder que les informations des villes de l'agglomération 
+	liste=[]
+	if annee < 2020 :													#pour différencier les fichiers, l'ordre des colonnes n'est pas le même partout
+		for ligne in donnees :
+			if ligne[2].isdigit() and int(ligne[2]) == code_dep and ligne[6] in agglo:	#Si le numéro est un chiffre, on vérifie si le déartement est 89 et on ne garde que les villes de l'agglomération totale
+				liste.append([ligne[6],int(ligne[7]),int(ligne[8]),int(ligne[9])])		#la liste contient dans l'ordre : nom de la commune, population municipale, population comptée à part et population totale
+	else :
+		for ligne in donnees :
+			if ligne[1].isdigit() and int(ligne[1]) == code_dep:		#Si le numéro est un chiffre, on vérifie si le déartement est 89 
+				for communes in meta :									#On ouvre la liste qui contient les numéros et les noms des communes 
+					if ligne[2] == communes[2] and communes[3] in agglo:	#On fait correspondre la numéro au nom de la commune, si la commune est dans l'agglomération:
+						liste.append([communes[3],int(ligne[3]),int(ligne[4]),int(ligne[5])])	#la liste contient dans l'ordre : nom de la commune, population municipale, population comptée à part et population totale
+	return liste
 
-agglo_immediate = ["Appoigny", "Auxerre", "Monéteau", "Perrigny", "Saint-Georges-sur-Baulche"]
-agglo_total = ["Appoigny", "Augy", "Auxerre", "Bleigny-le-Carreau", "Branches", "Champs-sur-Yonne", "Charbuy", "Chevannes", "Chitry", "Coulanges-la-Vineuse", "Escamps", "Escolives-Sainte-Camille", "Gurgy", "Gy-l'Évêque", "Irancy", "Jussy", "Lindry", "Monéteau", "Montigny-la-Resle", "Perrigny", "Quenne", "Saint-Bris-le-Vineux", "Saint-Georges-sur-Baulche", "Vallan", "Venoy", "Villefargeau", "Villeneuve-Saint-Salves", "Vincelles", "Vincelottes"]
+agglo_total=["Appoigny", "Augy", "Auxerre", "Bleigny-le-Carreau", "Branches", "Champs-sur-Yonne", "Charbuy", "Chevannes", "Chitry", "Coulanges-la-Vineuse", "Escamps", "Escolives-Sainte-Camille", "Gurgy","Gy-l'Évêque", "Irancy", "Jussy", "Lindry", "Monéteau", "Montigny-la-Resle", "Perrigny", "Quenne", "Saint-Bris-le-Vineux", "Saint-Georges-sur-Baulche", "Vallan", "Venoy", "Villefargeau", "Villeneuve-Saint-Salves", "Vincelles", "Vincelottes"]
+agglo_imm=["Appoigny","Auxerre", "Monéteau", "Perrigny", "Saint-Georges-sur-Baulche"]
 
-donnees_2008_auxerre = selec_region(donnees_2008, 2008, 89, metadonnees_communes, ["Auxerre"])
-donnees_2016_auxerre = selec_region(donnees_2016, 2016, 89, metadonnees_communes, ["Auxerre"])
-donnees_2021_auxerre = selec_region(donnees_2021, 2021, 89, metadonnees_communes, ["Auxerre"])
+donnees_2008=selec_region(donnees_2008, 2008,89, metadonnees_communes,agglo_total)
+donnees_2016=selec_region(donnees_2016, 2016,89, metadonnees_communes,agglo_total)
+donnees_2021=selec_region(donnees_2021, 2021,89, metadonnees_communes,agglo_total)
+#On a maintenant 3 listes qui contiennent dans l'ordre : nom de la commune, population municipale, population comptée à part et population totale
 
-donnees_2008_immediate = selec_region(donnees_2008, 2008, 89, metadonnees_communes, agglo_immediate)
-donnees_2016_immediate = selec_region(donnees_2016, 2016, 89, metadonnees_communes, agglo_immediate)
-donnees_2021_immediate = selec_region(donnees_2021, 2021, 89, metadonnees_communes, agglo_immediate)
+donnees=[]			
+for i in range(29):
+	donnees.append([donnees_2008[i][0],donnees_2008[i][3],donnees_2016[i][3],donnees_2021[i][3]])
 
-donnees_2008_total = selec_region(donnees_2008, 2008, 89, metadonnees_communes, agglo_total)
-donnees_2016_total = selec_region(donnees_2016, 2016, 89, metadonnees_communes, agglo_total)
-donnees_2021_total = selec_region(donnees_2021, 2021, 89, metadonnees_communes, agglo_total)
+donnees_agglo=[0,0,0]
+for commune in donnees:
+	for annee in range(3):
+		donnees_agglo[annee]=donnees_agglo[annee]+commune[annee+1]
+
+donnees_agglo_imm=[0,0,0]
+for commune in donnees:
+	for annee in range(3):
+		if commune[0] in agglo_imm:
+			donnees_agglo_imm[annee]=donnees_agglo_imm[annee]+commune[annee+1]
+			
+donnees_aux=[0,0,0]
+for commune in donnees:
+	for annee in range(3):
+		if commune[0] == "Auxerre":
+			donnees_aux[annee]=commune[annee+1]
+
+annees=[2008,2016,2021]
+print(donnees_agglo,donnees_agglo_imm,donnees_aux)
+figure("Evolution de la population") 
+plot(annees,donnees_agglo,label="Agglomération totale")
+plot(annees,donnees_agglo_imm,label="l'agglomération immédiate")
+plot(annees,donnees_aux,label="Auxerre")
+plt.legend()
+show()
